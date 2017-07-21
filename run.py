acab76fdf49468d1d7f138294e986f1cdb7f8d9b
Donna Mwiine <donna.mwiine@andela.com>
2017-07-21 16:31:46 +0300

    Add configurations to support heroku deployment

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import os
 
 from api import create_app
 
-config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-app = create_app(config_name)
+config_name = os.getenv('APP_SETTINGS') 
+app = create_app(os.getenv('FLASK_CONFIG') or config_name)
 
 if __name__ == '__main__':
     app.run()
