421d6403909e24835574360a1b8928d6b5c85286
Donna Mwiine <donna.mwiine@andela.com>
2017-07-21 11:07:13 +0300

    Remove unnecessary commented code

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,4 @@
 import os
-from flask_api import FlaskAPI
-from flask_sqlalchemy import SQLAlchemy
 
 class Config(object):
     """Parent configuration class."""
@@ -16,7 +14,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/testdb'
     DEBUG = True
 
 class StagingConfig(Config):
@@ -34,15 +32,3 @@ app_config = {
     'staging': StagingConfig,
     'production': ProductionConfig,
 }
-
-config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-app = FlaskAPI(__name__, instance_relative_config=True)
-app.config.from_object(app_config[config_name])
-# app.config.from_pyfile('config.py')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# initialize sql-alchemy
-db = SQLAlchemy()
-#app = FlaskAPI()
-
-db.init_app(app)
